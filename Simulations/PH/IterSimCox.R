13686e1101ad60a92d36d1bbd2730dfb3531c74c
Daniel Nevo <danielnevo@gmail.com>
2017-10-03 16:03:38 -0400

    Add material for simulation summarized in Table 2

---
+++ Simulations/PH/IterSimCox.R
@@ -0,0 +1,94 @@
+### 
+
+### 29 June 17 - added SE calculations for carry and midI
+## 21 Sep 17 -- added compatability with the revised ICcalib package
+IterSimCox <- function(n.sample, mu, lambda, alpha, beta0, n.points, gamma.z, gamma.q, pts.for.ints, n.int = 5, order = 2)
+{
+  my.data <- ICcalib:::SimCoxIntervalCensCox(n.sample = n.sample, mu = mu, lambda = lambda, alpha = alpha, gamma.z = gamma.z, gamma.q = gamma.q,
+                                   beta0 = beta0, n.points = n.points)
+  Z <- my.data$Z
+  Q <- my.data$Q
+  obs.tm <- my.data$obs.tm
+  delta <- my.data$delta
+  case.times <- obs.tm[delta==1]
+  n.cases <- length(case.times)
+  w <- my.data$w
+  w.res <- my.data$w.res
+  
+  ##### Naive methods ########
+  
+  #### Create data frame with time-dependent covariate suitable for coxph (for naive methods, see LVCFdata and MIdIdata) #######
+  
+  df.lvcf <- LVCFdata(w = w, w.res = w.res, obs.tm = obs.tm, delta = delta, Z = Z)
+  df.midI <- MidIdata(w = w, w.res = w.res, obs.tm = obs.tm, delta = delta, Z = Z)
+  
+  fit.lvcf <- survival::coxph(survival::Surv(start.time, stop.time, delta) ~ X + Z1 + Z2 + Z3, data = df.lvcf)
+  fit.midI <- survival::coxph(survival::Surv(start.time, stop.time, delta) ~ X + Z1 + Z2 + Z3, data = df.midI)
+  
+  est.lvcf <- coef(fit.lvcf)
+  est.midI <- coef(fit.midI)
+  
+  var.lvcf <- diag(fit.lvcf$var)
+  var.midI <- diag(fit.midI$var)
+  
+  ##### Fit calibration and risk set calibration models and then calculate P(X(t)=1|history)       ############
+  ###### for each person in the risk set, for all risk sets                                        ############
+  cox.hz.times <- sort(unique(obs.tm))
+  fit.cox <- tryCatch(FitCalibCox(w = w, w.res = w.res, Q = Q, hz.times = cox.hz.times, n.int = n.int, order = order), error = function(e){e})
+  fit.cox.fail <- 0
+  if(inherits(fit.cox, "error")){ fit.cox.fail <- 1 }
+  n.fits <- length(pts.for.ints)
+  fit.cox.rs.ints <- tryCatch(FitCalibCoxRSInts(w = w, w.res = w.res, Q = Q, hz.times = cox.hz.times, tm = obs.tm, event = delta, 
+                                                pts.for.ints = pts.for.ints, n.int = n.int, order = order),           error = function(e){e})
+  fit.cox.rs.ints.fail <- 0 
+  if(inherits(fit.cox.rs.ints, "error")){ fit.cox.rs.ints.fail <- 1 } else {
+    n.etas.per.fit <- vector(length = n.fits)
+    for (j in 1:n.fits)
+    {
+      n.etas.per.fit[j] <- length(fit.cox.rs.ints[[j]]$b) + length(fit.cox.rs.ints[[j]]$g)
+    }
+  }
+  est.cox.fail <- 0
+  if (fit.cox.fail==0)
+  {
+  px.cox <- t(sapply(case.times, ICcalib:::CalcCoxCalibP, w = w, w.res =  w.res, fit.cox, hz.times = cox.hz.times,  Q = Q))
+  px.cox.deriv <- sapply(case.times, ICcalib:::CalcCoxCalibPderiv, w = w, w.res =  w.res, fit.cox, hz.times = cox.hz.times,  Q = Q, simplify = "array")
+  est.cox <- tryCatch(optim(par = rep(0,ncol(Z)+1),fn = ICcalib:::CoxLogLik,  tm = obs.tm, event = delta, ps = px.cox, Z = Z,
+                            control = list(fnscale=-1))$par, error = function(e){e})
+  if(inherits(est.cox, "error")){
+    est.cox.fail <- 1
+  } else {
+    var.cox <- diag(ICcalib:::CalcVarParam(theta = est.cox, tm = obs.tm, event = delta, Z = Z, Q = Q,
+                            ps = px.cox, ps.deriv = px.cox.deriv, w = w, w.res = w.res, fit.cox = fit.cox))
+    }}
+  ##
+  est.cox.rs.fail <- 0
+  if (fit.cox.rs.ints.fail==0) {
+  px.cox.rs.ints <- t(sapply(case.times, ICcalib:::CalcCoxCalibRSIntsP, w = w, w.res =  w.res, fit.cox.rs.ints = fit.cox.rs.ints, 
+                             hz.times = cox.hz.times,  Q = Q, pts.for.ints = pts.for.ints))
+  px.cox.rs.ints.deriv <- sapply(case.times, ICcalib:::CalcCoxCalibPderivRSInsts, w = w, w.res =  w.res, fit.cox.rs.ints = fit.cox.rs.ints, 
+                                 hz.times = cox.hz.times,  Q = Q,  tm = obs.tm,  pts.for.ints = pts.for.ints, n.etas.per.fit = n.etas.per.fit,
+                                 simplify = "array")
+  est.cox.rs <- tryCatch(optim(par = rep(0,ncol(Z)+1),fn = ICcalib:::CoxLogLik,  tm = obs.tm, event = delta, ps = px.cox.rs.ints, Z = Z,
+                            control = list(fnscale=-1))$par, error = function(e){e})
+  if(inherits(est.cox.rs, "error")){
+    est.cox.rs.fail <- 1
+  } else {
+    var.cox.rs.ints <- ICcalib:::CalcVarParamRSInts(theta = est.cox.rs, tm = obs.tm, event = delta, Z = Z, Q = Q,
+                            ps = px.cox.rs.ints, ps.deriv = px.cox.rs.ints.deriv, w = w, w.res = w.res, fit.cox.rs.ints = fit.cox.rs.ints,
+                            pts.for.ints = pts.for.ints, n.etas.per.fit = n.etas.per.fit)
+    var.cox.rs <- diag(var.cox.rs.ints)
+    }
+  }
+  ###
+  
+  
+  # Returns a lot of things: first row are design parameters, second row are parameter estimates 
+  # third row are variance estimates, CI using estimated variance can be calculated when summarizing data
+  return.vec <- c(n.sample, mu, lambda, alpha,  beta0, n.points, n.cases, 
+                  est.lvcf, est.midI, est.cox, est.cox.rs,
+                  var.lvcf, var.midI, var.cox, var.cox.rs)
+  return(return.vec)
+}
+
+
---
+++ Simulations/PH/Scripts/CPBcoxBeta0.2w2n1000.R
@@ -0,0 +1,47 @@
+# Sep 21 ,2017
+# Daniel Nevo
+rm(list=ls())
+library(foreach)
+library(doParallel)
+library(doRNG)
+
+setwd("/home/dn84/ChangePointBin/Sims/Raux")
+source('IterSimCox.R')
+
+
+setwd("/home/dn84/ChangePointBin/Sims")
+
+# True Paramter Values
+mu = 0.2 # Exponential censoring paramter (mean=10)
+lambda = 0.1 # Scale for the time-to-event gompartz distribution
+alpha = 0.25 # Shape for the time-to-event gompartz distribution
+gamma.q = c(log(0.75), log(2.5))
+gamma.z = log(1.5)
+beta0 <- log(0.2)
+n.points <- 2
+pts.for.ints <- seq(0,4.5,0.25)
+n.int <- 10
+order <- 3
+n.sample <- 1000
+n.sim <- 1000
+
+
+
+cl <- makeCluster(12,outfile="")
+registerDoParallel(cl)
+
+my.seeds <- read.table("Raux/seeds.cox.txt",header = T, check.names = F)
+seed <- my.seeds[which(as.numeric(rownames(my.seeds))==n.points), which(round(as.numeric(colnames(my.seeds)),1)==round(exp(beta0),1))] 
+set.seed(seed)
+
+Results <- foreach(i = 1:n.sim,.packages = c("ICcalib"), .combine = "rbind")   %dorng% {
+  cat("i = ", i, "\n")                                              
+       IterSimCox(n.sample = n.sample, mu = mu, lambda = lambda, alpha = alpha, beta0 = beta0, n.points = n.points, 
+                  gamma.z = gamma.z, gamma.q = gamma.q, pts.for.ints = pts.for.ints,  n.int = n.int, order = order)
+}
+stopCluster(cl)
+save.image(paste0("CPBcoxBeta",exp(beta0),"W",n.points,"N",n.sample,".RData"))
+write.table(Results, paste0("CPBcoxBeta",exp(beta0),"W",n.points,"N",n.sample,".txt"))
+sessionInfo()
+
+
